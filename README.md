95901b8bf5b718bce53e835f2a18cf12cde8ebd9
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-10-11 14:42:25 +0000

    CRUD estruturado

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SENAI-91210-bd-python-2
+
+## Objetivo:
+Criar CRUD com Python em uma estrutura de pacotes.
+
+### Tecnologias:
+- ORM: SQLAlchemy
+- Docker: Banco de dados MySQL
---
+++ app/services/usuario_services.py
@@ -0,0 +1,19 @@
+from models.usuario import Usuario
+from repositories.usuario_repository import UsuarioRepository
+
+class UsuarioService:
+    def __init__(self, repository: UsuarioRepository) -> None:
+        self.repository = repository
+
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try:
+            usuario = Usuario(nome= nome, email= email, senha= senha)
+            self.repository.salvar_usuario(usuario)
+            print("Usuário salvo com sucesso!")
+        except TypeError as error:
+            print("Erro ao salvar arquivo: {error}.")
+        except Exception as error:
+            print("Ocorreu um erro inesperado: {error}.")
+
+    def listar_todos_usuarios(self):
+        return self.repository.listar_todos_usuario()
